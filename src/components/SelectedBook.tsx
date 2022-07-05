119fef32647928b7f7dd9c872821a38d5d3b7918
Sergey Belokrylets <s.belokrylets@gmail.com>
2022-07-05 15:21:09 +0300

    завершен компонент SelectedBook.tsx

--- src/components/SelectedBook.tsx
+++ src/components/SelectedBook.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
-const SelectedBook = () => {
-  return (
-    <div>SelectedBook</div>
-  )
+interface SelectedBookProps {
+    selectedBookImage: string;
+    path: string;
+    selectedBookTitle: string;
+    selectedBookAuthor: string;
+    selectedDescription: string;
+}
+const SelectedBook = ({ selectedBookImage, path, selectedBookTitle, selectedBookAuthor, selectedDescription }: SelectedBookProps) => {
+    return (
+        <div className="selectedBook">
+            <div className="selectedBookImage">
+                <img src={selectedBookImage} alt="" />
+            </div>
+            <div className="selectedBookInfo">
+                <div className="path">
+                    {path}
+                </div>
+                <div className="selectedBookTitle">
+                    {selectedBookTitle}
+                </div>
+                <div className="selectedBookAuthor">
+                    {selectedBookAuthor}
+                </div>
+                <div className="selectedDescription">
+                    {selectedDescription}
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default SelectedBook;
